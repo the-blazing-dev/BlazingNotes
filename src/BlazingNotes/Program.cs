ef9926d441671b9cdf1dbe746e764e280866efb5
David Ritter <blazingdev@outlook.com>
2024-07-14 22:48:44 +0200

    AppDb connection string from appsettings

--- src/BlazingNotes/Program.cs
+++ src/BlazingNotes/Program.cs
@@ -12,14 +12,14 @@ builder.Services.AddRazorComponents()
 builder.Services.AddMudServices();
 builder.Services.AddFluxor(x => x.ScanAssemblies(Assembly.GetExecutingAssembly(), typeof(AppState).Assembly));
 
-builder.Services.AddSqlite<AppDb>("Data Source=../appdb.sqlite");
+builder.Services.AddSqlite<AppDb>(builder.Configuration.GetConnectionString("AppDb"));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Error", true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
