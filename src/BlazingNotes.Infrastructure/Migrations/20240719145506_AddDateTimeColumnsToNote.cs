676d736108abbb0c22fd7e43805b397e1b7f5914
David Ritter <blazingdev@outlook.com>
2024-07-19 19:40:58 +0200

    migration: AddDateTimeColumnsToNote

---
+++ src/BlazingNotes.Infrastructure/Migrations/20240719145506_AddDateTimeColumnsToNote.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazingNotes.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDateTimeColumnsToNote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // manually reordered the statements in hope to have the same ordering in the database
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Notes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(2000,01,01, 0,0,0,DateTimeKind.Utc)); // configured manually
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Notes",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RelevantAt",
+                table: "Notes",
+                type: "TEXT",
+                nullable: true);
+            
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ArchivedAt",
+                table: "Notes",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Notes",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ArchivedAt",
+                table: "Notes");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Notes");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Notes");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Notes");
+
+            migrationBuilder.DropColumn(
+                name: "RelevantAt",
+                table: "Notes");
+        }
+    }
+}
--- src/BlazingNotes.Logic/Entities/Note.cs
+++ src/BlazingNotes.Logic/Entities/Note.cs
@@ -5,4 +5,10 @@ public class Note
     public Guid Id { get; set; } = Guid.NewGuid();
     public required string Text { get; set; }
     public bool IsArchived { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? ModifiedAt { get; set; }
+    public DateTime? RelevantAt { get; set; }
+    public DateTime? ArchivedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
 }
