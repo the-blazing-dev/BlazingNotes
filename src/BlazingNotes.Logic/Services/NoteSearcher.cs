330dcbc1c1051dd501208d9d9297c0c5550da2b4
David Ritter <blazingdev@outlook.com>
2024-07-30 22:23:25 +0200

    NoteCardListSeparated which automatically creates an Archived section

--- src/BlazingNotes.Logic/Services/NoteSearcher.cs
+++ src/BlazingNotes.Logic/Services/NoteSearcher.cs
@@ -5,10 +5,15 @@ public static class NoteSearcher
     public static (bool longEnough, string highlightText, ICollection<Note> results) Search(ICollection<Note> notes,
         string searchTerm)
     {
-        var search = searchTerm.Trim();
-        if (search.Length <= 2) return (false, "", notes);
+        if (!IsSearchTermLongEnough(searchTerm)) return (false, "", notes);
 
+        var search = searchTerm.Trim();
         var matches = notes.Where(x => x.Text.ContainsIgnoreCase(searchTerm)).ToList();
         return (true, search, matches);
+    }
+
+    public static bool IsSearchTermLongEnough(string search)
+    {
+        return search.Trim().Length > 2;
     }
 }
