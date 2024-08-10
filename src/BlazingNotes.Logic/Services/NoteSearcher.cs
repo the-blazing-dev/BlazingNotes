50f7801101cf0c648694003cc7c7d9e0cec65415
David Ritter <blazingdev@outlook.com>
2024-08-10 22:21:29 +0200

    use BzSearchQuery instead of simple string

--- src/BlazingNotes.Logic/Services/NoteSearcher.cs
+++ src/BlazingNotes.Logic/Services/NoteSearcher.cs
@@ -1,19 +1,22 @@
+using BlazingNotes.Logic.Search;
+
 namespace BlazingNotes.Logic.Services;
 
 public static class NoteSearcher
 {
-    public static (bool longEnough, string highlightText, ICollection<Note> results) Search(ICollection<Note> notes,
+    public static (bool longEnough, BzSearchQuery query, ICollection<Note> results) Search(ICollection<Note> notes,
         string searchTerm)
     {
-        if (!IsSearchTermLongEnough(searchTerm)) return (false, "", notes);
+        var query = new BzSearchQuery(searchTerm);
+        if (!IsSearchTermLongEnough(searchTerm)) return (false, query, notes);
 
-        var search = searchTerm.Trim();
-        var matches = notes.Where(x => x.Text.ContainsIgnoreCase(searchTerm)).ToList();
-        return (true, search, matches);
+        var matches = notes.Where(x => BzSearch.IsMatch(query, x.Text)).ToList();
+
+        return (true, query, matches);
     }
 
     public static bool IsSearchTermLongEnough(string search)
     {
-        return search.Trim().Length > 2;
+        return search.Trim().Length >= 3;
     }
 }
