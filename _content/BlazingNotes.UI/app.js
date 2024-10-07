f2766512d2b62c3e0dc969dd094e016ad13c8ce2
the-blazing-dev <blazingdev@outlook.com>
2024-10-07 20:55:43 +0000

    Deploying to gh-pages from @ the-blazing-dev/BlazingNotes@97353a5e3c58eb354b586b350afae9bcf851dfe7 🚀

---
+++ _content/BlazingNotes.UI/app.js
@@ -0,0 +1,7 @@
+function addHashtagCharacter(el) {
+    const start = el.selectionStart;
+    const value = el.value;
+    el.value = value.substring(0, start) + '#' + value.substring(start);
+    el.setSelectionRange(start + 1, start + 1);
+    el.focus();
+}
