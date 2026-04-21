ba198a21a19b713b00a648e2307260b0f79b2807
devritter <devritter@hotmail.com>
2026-04-21 12:13:39 +0200

    optimize renderings, finally support CMD+Enter

---
+++ src/BlazingNotes.UI/Components/BzEventUtil.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Components;
+
+namespace BlazingNotes.UI.Components;
+
+public static class BzEventUtil
+{
+    public static Action AsNonRenderingEventHandler(Action callback)
+        => new SyncReceiver(callback).Invoke;
+    public static Action<TValue> AsNonRenderingEventHandler<TValue>(
+        Action<TValue> callback)
+        => new SyncReceiver<TValue>(callback).Invoke;
+    public static Func<Task> AsNonRenderingEventHandler(Func<Task> callback)
+        => new AsyncReceiver(callback).Invoke;
+    public static Func<TValue, Task> AsNonRenderingEventHandler<TValue>(
+        Func<TValue, Task> callback)
+        => new AsyncReceiver<TValue>(callback).Invoke;
+
+    private record SyncReceiver(Action Callback) : ReceiverBase
+    {
+        public void Invoke() => Callback();
+    }
+
+    private record SyncReceiver<T>(Action<T> Callback) : ReceiverBase
+    {
+        public void Invoke(T arg) => Callback(arg);
+    }
+
+    private record AsyncReceiver(Func<Task> Callback) : ReceiverBase
+    {
+        public Task Invoke() => Callback();
+    }
+
+    private record AsyncReceiver<T>(Func<T, Task> Callback) : ReceiverBase
+    {
+        public Task Invoke(T arg) => Callback(arg);
+    }
+
+    private abstract record ReceiverBase : IHandleEvent
+    {
+        public Task HandleEventAsync(EventCallbackWorkItem item, object? arg) => 
+            item.InvokeAsync(arg);
+    }
+}
